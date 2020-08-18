1c3c8bd060937f336ea6dd0151c660b41dcd57e8
Gareth Coles <gdude2002@gmail.com>
2020-08-18 13:47:05 +0100

    Fix up StopModReposts code - they fixed the API!

--- src/main/kotlin/info/journeymap/mappy/stopmodreposts/ModRepostsSpec.kt
+++ src/main/kotlin/info/journeymap/mappy/stopmodreposts/ModRepostsSpec.kt
@@ -3,6 +3,6 @@ package info.journeymap.mappy.stopmodreposts
 import com.uchuhimo.konf.ConfigSpec
 
 /** Config spec representing the data obtained from StopModReposts. **/
-object ModRepostsSpec : ConfigSpec() {
-    val sites by required<List<ModReposts>>()
+object ModRepostsSpec : ConfigSpec("") {  // There's no prefix for this one
+    val sites by required<List<ModReposts>>(name = "sites")
 }
